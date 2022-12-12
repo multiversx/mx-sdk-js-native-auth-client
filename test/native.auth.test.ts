7086c84e4c8a27d490d5b5d1a02bb434c6c4865b
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-12-12 17:42:17 +0200

    SERVICES-1026: fix tests

--- test/native.auth.test.ts
+++ test/native.auth.test.ts
@@ -1,35 +1,18 @@
 import axios from "axios";
 import MockAdapter, { RequestHandler } from "axios-mock-adapter";
-import { NativeAuthClient } from "../src/native.auth.client";
+import { NativeAuthClient } from '../src';
 
 describe("Native Auth", () => {
   let mock: MockAdapter;
   const ADDRESS = 'erd13rrn3fwjds8r5260n6q3pd2qa6wqkudrhczh26d957c0edyzermshds0k8';
-  const HOST = 'elrond.com';
   const SIGNATURE = '4b445f287663b868e269aa0532c9fd73acb37cfd45f46e33995777e68e5ecc15d97318d9af09c4102f9b40ecf347a75e2d2e81acbcc3c72ae32fcf659c2acd0e';
   const BLOCK_HASH = 'b3d07565293fd5684c97d2b96eb862d124fd698678f3f95b2515ed07178a27b4';
   const TTL = 86400;
-  const TOKEN = `ZWxyb25kLmNvbQ.${BLOCK_HASH}.${TTL}.e30`;
-  const ACCESS_TOKEN = 'ZXJkMTNycm4zZndqZHM4cjUyNjBuNnEzcGQycWE2d3FrdWRyaGN6aDI2ZDk1N2MwZWR5emVybXNoZHMwazg.Wld4eWIyNWtMbU52YlEuYjNkMDc1NjUyOTNmZDU2ODRjOTdkMmI5NmViODYyZDEyNGZkNjk4Njc4ZjNmOTViMjUxNWVkMDcxNzhhMjdiNC44NjQwMC5lMzA.4b445f287663b868e269aa0532c9fd73acb37cfd45f46e33995777e68e5ecc15d97318d9af09c4102f9b40ecf347a75e2d2e81acbcc3c72ae32fcf659c2acd0e';
-  const BLOCK_TIMESTAMP = 1653068466;
-
-  const PEM_KEY = `-----BEGIN PRIVATE KEY for erd1qnk2vmuqywfqtdnkmauvpm8ls0xh00k8xeupuaf6cm6cd4rx89qqz0ppgl-----
-  ODY1NmI0ZjMzYTRjOTY0MGI3MTFiY2E4NDUzODNiMDZiNjczMjAzNjk2ZjYxYjMy
-  N2E5MDY3ODdlNWExODg1NjA0ZWNhNjZmODAyMzkyMDViNjc2ZGY3OGMwZWNmZjgz
-  Y2Q3N2JlYzczNjc4MWU3NTNhYzZmNTg2ZDQ2NjM5NDA=
-  -----END PRIVATE KEY for erd1qnk2vmuqywfqtdnkmauvpm8ls0xh00k8xeupuaf6cm6cd4rx89qqz0ppgl-----`;
-  const PEM_ADDRESS = 'erd1qnk2vmuqywfqtdnkmauvpm8ls0xh00k8xeupuaf6cm6cd4rx89qqz0ppgl';
+  const TOKEN = `${BLOCK_HASH}.${TTL}.e30`;
+  const ACCESS_TOKEN = 'ZXJkMTNycm4zZndqZHM4cjUyNjBuNnEzcGQycWE2d3FrdWRyaGN6aDI2ZDk1N2MwZWR5emVybXNoZHMwazg.YjNkMDc1NjUyOTNmZDU2ODRjOTdkMmI5NmViODYyZDEyNGZkNjk4Njc4ZjNmOTViMjUxNWVkMDcxNzhhMjdiNC44NjQwMC5lMzA.4b445f287663b868e269aa0532c9fd73acb37cfd45f46e33995777e68e5ecc15d97318d9af09c4102f9b40ecf347a75e2d2e81acbcc3c72ae32fcf659c2acd0e';
 
   const onLatestBlockHashGet = function (mock: MockAdapter): RequestHandler {
     return mock.onGet('https://api.elrond.com/blocks?size=1&fields=hash');
-  };
-
-  const onLatestBlockTimestampGet = function (mock: MockAdapter): RequestHandler {
-    return mock.onGet('https://api.elrond.com/blocks?size=1&fields=timestamp');
-  };
-
-  const onSpecificBlockTimestampGet = function (mock: MockAdapter): RequestHandler {
-    return mock.onGet(`https://api.elrond.com/blocks/${BLOCK_HASH}?extract=timestamp`);
   };
 
   beforeAll(() => {
@@ -42,9 +25,7 @@ describe("Native Auth", () => {
 
   describe("Client", () => {
     it("Latest block should return signable token", async () => {
-      const client = new NativeAuthClient({
-        host: HOST,
-      });
+      const client = new NativeAuthClient({});
 
       onLatestBlockHashGet(mock).reply(200, [{ hash: BLOCK_HASH }]);
 
