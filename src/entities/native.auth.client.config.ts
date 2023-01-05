408358467719f1e925d3ec37ccf21ef6248e94dc
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2023-01-05 19:27:09 +0200

    SERVICES-1125: JS Native Auth gateway implementation

--- src/entities/native.auth.client.config.ts
+++ src/entities/native.auth.client.config.ts
@@ -3,4 +3,5 @@ export class NativeAuthClientConfig {
   apiUrl: string = 'https://api.elrond.com';
   expirySeconds: number = 60 * 60 * 24;
   blockHashShard?: number;
+  gatewayUrl?: string;
 }
--- test/native.auth.test.ts
+++ test/native.auth.test.ts
@@ -58,3 +58,89 @@ describe("Native Auth", () => {
     });
   });
 });
+
+describe("Native Auth with gateway", () => {
+  let mock: MockAdapter;
+  const ADDRESS = 'erd13rrn3fwjds8r5260n6q3pd2qa6wqkudrhczh26d957c0edyzermshds0k8';
+  const SIGNATURE = '4b445f287663b868e269aa0532c9fd73acb37cfd45f46e33995777e68e5ecc15d97318d9af09c4102f9b40ecf347a75e2d2e81acbcc3c72ae32fcf659c2acd0e';
+  const BLOCK_HASH = 'b3d07565293fd5684c97d2b96eb862d124fd698678f3f95b2515ed07178a27b4';
+  const TTL = 86400;
+  const HOST = 'elrond.com';
+  const TOKEN = `ZWxyb25kLmNvbQ.${BLOCK_HASH}.${TTL}.e30`;
+  const ACCESS_TOKEN = 'ZXJkMTNycm4zZndqZHM4cjUyNjBuNnEzcGQycWE2d3FrdWRyaGN6aDI2ZDk1N2MwZWR5emVybXNoZHMwazg.Wld4eWIyNWtMbU52YlEuYjNkMDc1NjUyOTNmZDU2ODRjOTdkMmI5NmViODYyZDEyNGZkNjk4Njc4ZjNmOTViMjUxNWVkMDcxNzhhMjdiNC44NjQwMC5lMzA.4b445f287663b868e269aa0532c9fd73acb37cfd45f46e33995777e68e5ecc15d97318d9af09c4102f9b40ecf347a75e2d2e81acbcc3c72ae32fcf659c2acd0e';
+  const latestRound = 115656;
+  const METASHARD = 4294967295;
+  const GATEWAY = 'https://gateway.elrond.com';
+
+  const onBlocksByRound = function (mock: MockAdapter): RequestHandler {
+    return mock.onGet(`${GATEWAY}/blocks/by-round/${latestRound}`);
+  };
+
+  //https://testnet-gateway.elrond.com/network/status/4294967295
+  const onNetworkStatus = function (mock: MockAdapter): RequestHandler {
+    return mock.onGet(`${GATEWAY}/network/status/${METASHARD}`);
+  };
+
+  beforeAll(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mock.reset();
+  });
+
+  describe("Client", () => {
+    it("Latest block should return signable token", async () => {
+      const client = new NativeAuthClient({
+        host: HOST,
+        gatewayUrl: GATEWAY,
+        blockHashShard: METASHARD,
+      });
+
+      onNetworkStatus(mock).reply(200, { data: { status: {erd_current_round: latestRound}} });
+      onBlocksByRound(mock).reply(200, { data: { blocks: [ {shard: METASHARD, hash: BLOCK_HASH}] }});
+
+      const token = await client.initialize();
+
+      expect(token).toStrictEqual(TOKEN);
+    });
+
+    it("Internal server error", async () => {
+      const client = new NativeAuthClient({
+        gatewayUrl: GATEWAY,
+        blockHashShard: METASHARD,
+      });
+
+      onNetworkStatus(mock).reply(500);
+
+      await expect(client.initialize()).rejects.toThrow();
+    });
+
+    it("Internal server error", async () => {
+      const client = new NativeAuthClient({
+        gatewayUrl: GATEWAY,
+        blockHashShard: METASHARD,
+      });
+
+      onNetworkStatus(mock).reply(200, [{ data: { status: {erd_current_round: latestRound}} }]);
+      onBlocksByRound(mock).reply(500);
+
+      await expect(client.initialize()).rejects.toThrow();
+    });
+
+    it('Generate Access token', () => {
+      const client = new NativeAuthClient({
+        gatewayUrl: GATEWAY,
+        blockHashShard: METASHARD,
+      });
+
+      const accessToken = client.getToken(
+          ADDRESS,
+          TOKEN,
+          SIGNATURE
+      );
+
+      expect(accessToken).toStrictEqual(ACCESS_TOKEN);
+    });
+  });
+});
