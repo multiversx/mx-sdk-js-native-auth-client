e6bd33f528fec5b81a463861442bfd912aedb10d
johnykes <johnslymusic@gmail.com>
2023-01-10 15:05:34 +0200

    SERVICES-1131 rebranding

--- src/entities/native.auth.client.config.ts
+++ src/entities/native.auth.client.config.ts
@@ -1,6 +1,6 @@
 export class NativeAuthClientConfig {
   host: string = typeof window !== "undefined" ? window.location.hostname : '';
-  apiUrl: string = 'https://api.elrond.com';
+  apiUrl: string = 'https://api.multiversx.com';
   expirySeconds: number = 60 * 60 * 24;
   blockHashShard?: number;
   gatewayUrl?: string;
