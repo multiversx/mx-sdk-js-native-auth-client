1a47750f6b2fc57ec3dc1915ecc63eac4b450c5d
Traian Anghel <tanghel@live.com>
2023-02-15 13:55:55 +0200

    rename host to origin

--- test/native.auth.test.ts
+++ test/native.auth.test.ts
@@ -8,7 +8,7 @@ describe("Native Auth", () => {
   const SIGNATURE = '563cb2dfdf96ab335423a05287fa3cd00154034423d0062421ee6ce03230d941da6df9ce79689fcd173c0ba5d4331b3ccd82c8ec2e6ab4d875db1587c2ab720c';
   const BLOCK_HASH = '82ec8044966efb2d00e8a6367ea23ddbc7bea6504ed98f4a1a536d7c21bb2682';
   const TTL = 86400;
-  const HOST = 'api.multiversx.com';
+  const ORIGIN = 'https://api.multiversx.com';
   const TOKEN = `YXBpLm11bHRpdmVyc3guY29t.${BLOCK_HASH}.${TTL}.e30`;
   const ACCESS_TOKEN = 'ZXJkMXFuazJ2bXVxeXdmcXRkbmttYXV2cG04bHMweGgwMGs4eGV1cHVhZjZjbTZjZDRyeDg5cXF6MHBwZ2w.WVhCcExtMTFiSFJwZG1WeWMzZ3VZMjl0LjgyZWM4MDQ0OTY2ZWZiMmQwMGU4YTYzNjdlYTIzZGRiYzdiZWE2NTA0ZWQ5OGY0YTFhNTM2ZDdjMjFiYjI2ODIuODY0MDAuZTMw.563cb2dfdf96ab335423a05287fa3cd00154034423d0062421ee6ce03230d941da6df9ce79689fcd173c0ba5d4331b3ccd82c8ec2e6ab4d875db1587c2ab720c';
 
@@ -27,7 +27,7 @@ describe("Native Auth", () => {
   describe("Client", () => {
     it("Latest block should return signable token", async () => {
       const client = new NativeAuthClient({
-        host: HOST,
+        origin: ORIGIN,
       });
 
       onLatestBlockHashGet(mock).reply(200, [{ hash: BLOCK_HASH }]);
@@ -65,7 +65,7 @@ describe("Native Auth with gateway", () => {
   const SIGNATURE = '563cb2dfdf96ab335423a05287fa3cd00154034423d0062421ee6ce03230d941da6df9ce79689fcd173c0ba5d4331b3ccd82c8ec2e6ab4d875db1587c2ab720c';
   const BLOCK_HASH = '82ec8044966efb2d00e8a6367ea23ddbc7bea6504ed98f4a1a536d7c21bb2682';
   const TTL = 86400;
-  const HOST = 'api.multiversx.com';
+  const ORIGIN = 'https://api.multiversx.com';
   const TOKEN = `YXBpLm11bHRpdmVyc3guY29t.${BLOCK_HASH}.${TTL}.e30`;
   const ACCESS_TOKEN = 'ZXJkMXFuazJ2bXVxeXdmcXRkbmttYXV2cG04bHMweGgwMGs4eGV1cHVhZjZjbTZjZDRyeDg5cXF6MHBwZ2w.WVhCcExtMTFiSFJwZG1WeWMzZ3VZMjl0LjgyZWM4MDQ0OTY2ZWZiMmQwMGU4YTYzNjdlYTIzZGRiYzdiZWE2NTA0ZWQ5OGY0YTFhNTM2ZDdjMjFiYjI2ODIuODY0MDAuZTMw.563cb2dfdf96ab335423a05287fa3cd00154034423d0062421ee6ce03230d941da6df9ce79689fcd173c0ba5d4331b3ccd82c8ec2e6ab4d875db1587c2ab720c';
   const latestRound = 115656;
@@ -92,13 +92,13 @@ describe("Native Auth with gateway", () => {
   describe("Client", () => {
     it("Latest block should return signable token", async () => {
       const client = new NativeAuthClient({
-        host: HOST,
+        origin: ORIGIN,
         gatewayUrl: GATEWAY,
         blockHashShard: METASHARD,
       });
 
-      onNetworkStatus(mock).reply(200, { data: { status: {erd_current_round: latestRound}} });
-      onBlocksByRound(mock).reply(200, { data: { blocks: [ {shard: METASHARD, hash: BLOCK_HASH}] }});
+      onNetworkStatus(mock).reply(200, { data: { status: { erd_current_round: latestRound } } });
+      onBlocksByRound(mock).reply(200, { data: { blocks: [{ shard: METASHARD, hash: BLOCK_HASH }] } });
 
       const token = await client.initialize();
 
@@ -122,7 +122,7 @@ describe("Native Auth with gateway", () => {
         blockHashShard: METASHARD,
       });
 
-      onNetworkStatus(mock).reply(200, [{ data: { status: {erd_current_round: latestRound}} }]);
+      onNetworkStatus(mock).reply(200, [{ data: { status: { erd_current_round: latestRound } } }]);
       onBlocksByRound(mock).reply(500);
 
       await expect(client.initialize()).rejects.toThrow();
@@ -135,9 +135,9 @@ describe("Native Auth with gateway", () => {
       });
 
       const accessToken = client.getToken(
-          ADDRESS,
-          TOKEN,
-          SIGNATURE
+        ADDRESS,
+        TOKEN,
+        SIGNATURE
       );
 
       expect(accessToken).toStrictEqual(ACCESS_TOKEN);
